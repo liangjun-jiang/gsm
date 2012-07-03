83ef4bf76ff24c7d3a1307fb69623dce326df443
Liangjun Jiang <ljiang@bypasslane.com>
2012-07-02 21:56:49 -0500

    add report view

---
+++ ReportViewController.h
@@ -0,0 +1,13 @@
+//
+//  ReportViewController.h
+//  AccelerometerGraph
+//
+//  Created by Liangjun Jiang on 7/2/12.
+//  Copyright (c) 2012 ByPass Lane. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ReportViewController : UITableViewController
+
+@end
